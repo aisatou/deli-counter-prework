c42016d721ed91afb9ba02db51f72c57545d1302
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-04-07 22:32:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,13 @@
 def line(katz_deli)
+  output = ""
   if katz_deli.size == 0
-    puts "The line is currently empty."
+    output = "The line is currently empty."
   else
-    puts "The line is currently: "
+    output += "The line is currently: "
     range = 0..katz_deli.size - 1
     for num in range
-      puts "#{num + 1}. #{katz_deli[num]} "
+      output += "#{num + 1}. #{katz_deli[num]} "
     end
   end
+  puts output
 end
